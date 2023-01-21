5c51d3b6bac424685b1592e74602d07e587dd1bb
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-01-21 07:16:08 -0800

    adding upto migrations & models

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,20 @@
 class MainController < ApplicationController
+  
   def index
+    #render('index')
   end
+
+  def about
+    @created_by = "Arvind"
+    @id = params['id']
+    @page = params[:page]
+    #render('about')
+
+  end
+
+  def hello
+    redirect_to(action: 'index')
+    #redirect_to('https://google.com') testing with direct url
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   def index
+    @page =params['page'].to_i
+    @tasks = ['laundry', 'clean porch', 'wash dishes', 'mow lawn']
   end
 
   def new
