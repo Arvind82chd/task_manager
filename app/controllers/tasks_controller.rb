ce840ca06bdba84b989485bdd43caad4ae149e23
Arvind82chd <58764578+Arvind82chd@users.noreply.github.com>
2023-01-26 16:50:41 -0800

    CRUD operations tested

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
   def index
-    @page =params['page'].to_i
-    @tasks = ['laundry', 'clean porch', 'wash dishes', 'mow lawn']
+    # @page =params['page'].to_i
+    # @tasks = ['laundry', 'clean porch', 'wash dishes', 'mow lawn']
+    @tasks = Task.order(:position) #instance variable set
   end
 
   def show
+    @task =Task.find(params[:id])
   end
 
   def new
